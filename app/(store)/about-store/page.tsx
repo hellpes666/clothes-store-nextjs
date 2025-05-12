c2d7a4f8363674db09a0928c15ba7eb6325aa174
Hellpes <alexrui_hpow@mail.ru>
2025-05-12 13:28:19 +0300

    feat: add hover effect to the header; feat: add responsivve for the about store page

--- app/(store)/about-store/page.tsx
+++ app/(store)/about-store/page.tsx
@@ -13,20 +13,20 @@ export const metadata: Metadata = {
 export default function AboutStore() {
 	return (
 		<div className="mx-auto max-w-4xl px-4 py-16 text-lg leading-relaxed md:text-xl">
-			<h1 className="mb-6 text-3xl font-bold text-secondary-900">О магазине</h1>
-			<p className="mb-4">
+			<h1 className="mb-6 text-3xl font-bold text-secondary-900 md:text-4xl">О магазине</h1>
+			<p className="mb-4 md:text-xl xl:text-2xl">
 				Добро пожаловать в учебный онлайн-магазин!
 				<br />
 				Этот проект был разработан в рамках учебной деятельности в{" "}
 				<strong>Российском технологическом университете — МИРЭА</strong>. Цель проекта — продемонстрировать
 				применение современных веб-технологий для создания полнофункционального интернет-магазина.
 			</p>
-			<p className="mb-4">
+			<p className="mb-4 md:text-xl xl:text-2xl">
 				Я постарались реализовать все ключевые возможности: просмотр каталога, детальные страницы товаров,
 				адаптивный интерфейс, а также приятный пользовательский опыт. Интерфейс разработан с использованием{" "}
 				<i>NextJS</i>, а данные получаются с помощью публичного API.
 			</p>
-			<p className="mb-4">
+			<p className="mb-4 md:text-xl xl:text-2xl">
 				Проект не является коммерческим и создан исключительно в образовательных целях для демонстрации навыков
 				разработки фронтенда и взаимодействия с API.
 			</p>
--- components/Header/store/Header.tsx
+++ components/Header/store/Header.tsx
@@ -4,11 +4,11 @@ import { Cart } from "./Cart";
 
 export const Header = () => {
 	return (
-		<header className="flex-items hidden justify-between md:flex md:py-2">
+		<header className="flex-items group hidden justify-between md:flex md:py-2">
 			<div className="flex items-center gap-3">
 				<Link
 					href={"/"}
-					className="mr-3 rounded bg-primary p-3 text-xl font-medium uppercase text-primary-foreground"
+					className="ransition-colors mr-3 rounded bg-primary p-3 text-xl font-medium uppercase text-primary-foreground duration-500 group-hover:text-white"
 				>
 					hell store
 				</Link>
